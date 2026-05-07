c20c961b0f5b47e25609ae0f8b1cd44e33025308
andyzarate63-hue <andyzarate63@gmail.com>
2026-05-06 23:58:57 -0600

    Update telegram_service.py

--- telegram_service.py
+++ telegram_service.py
@@ -13,21 +13,31 @@ def enviar_mensaje(lat, lon, device_id, timestamp):
     url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
     mensaje = (
-        f"📍 GPS TRACKER\n"
-        f"ID: {device_id}\n"
-        f"Hora: {timestamp}\n"
-        f"Lat: {lat}\n"
-        f"Lon: {lon}\n"
-        f"Mapa: https://maps.google.com/?q={lat},{lon}"
+        f"📍 GPS TRACKER\n\n"
+        f"🆔 ID: {device_id}\n"
+        f"🛰 Satélites: GPS ACTIVE\n"
+        f"🕒 Hora UTC: {timestamp}\n\n"
+        f"🌍 Latitud: {lat}\n"
+        f"🌍 Longitud: {lon}\n\n"
+        f"📌 Google Maps:\n"
+        f"https://maps.google.com/?q={lat},{lon}"
     )
 
     try:
-        r = requests.post(url, json={
-            "chat_id": CHAT_ID,
-            "text": mensaje
-        }, timeout=10)
 
-        return r.status_code == 200
+        response = requests.post(
+            url,
+            json={
+                "chat_id": CHAT_ID,
+                "text": mensaje
+            },
+            timeout=10
+        )
 
-    except:
+        return response.status_code == 200
+
+    except Exception as e:
+
+        print(f"Telegram Error: {e}")
+
         return False
